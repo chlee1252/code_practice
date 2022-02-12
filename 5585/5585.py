f266dec9c55e19a7818f38572945ce21ee88bf1d
Marc <chlee1252@gmail.com>
2022-02-12 14:43:01 +0900

    심심해..

---
+++ 5585/5585.py
@@ -0,0 +1,13 @@
+money = 1000 - int(input())
+
+coins = [500, 100, 50, 10, 5, 1]
+
+result = 0
+for coin in coins:
+    if money <= 0:
+        break
+    if money >= coin:
+        result += money // coin
+        money %= coin
+
+print(result)
